25debb2f80c35003862c4f22c160a7c02b5cc36c
voduykhanh1981998 <voduykhanh1981998@users.noreply.github.com>
2019-09-15 00:37:27 +0700

    .

--- Tuan1/src/Lab3/Bai2.java
+++ Tuan1/src/Lab3/Bai2.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Lab3;
+package lab3;
 
 /**
  *
- * @author USER
+ * @author ASUS
  */
 public class Bai2 {
-    
+    public static void main(String[] args) {
+        for(int i=1; i<=9; i++){
+            System.out.println("------------");
+            for(int j=1; j<=10; j++){
+                System.out.println(+i+" x "+j+"="+i*j);
+    }
+    }
+    }
 }
--- Tuan1/src/Lab3/Bai3.java
+++ Tuan1/src/Lab3/Bai3.java
@@ -3,12 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Lab3;
+package lab3;
 
+import java.util.Scanner;
+import java.util.Arrays;
+import java.text.DecimalFormat;
+import org.w3c.dom.traversal.NodeFilter;
 /**
  *
- * @author USER
+ * @author ASUS
  */
 public class Bai3 {
-    
+    public static void main(String[] args) {
+        int n;
+        double TbCong, tong=0, diem=0;
+        Scanner duyKhanh =new Scanner(System.in);
+        System.out.println("Nhập số lượng các phần tử của mảng :");
+        n=duyKhanh.nextInt();
+        int []mang =new int[n];
+        for(int i=0; i<n; i++){
+            System.out.println("Nhập vào phần tử thứ a["+i+"] :");
+            mang[i]=duyKhanh.nextInt();
+    }
+       Arrays.sort(mang);
+        System.out.println("Các phần tử của mảng sau khi sắp xếp lại ");
+        for(int i=0; i<n; i++){
+            System.out.println(+mang[i]+" ");
+            
+        }
+        int min=mang[0];
+        for(int i=0; i<n; i++){
+            if (mang[i]%3 ==0 ){
+                tong=tong+mang[i];
+                diem++;
+            }
+        }
+        System.out.println("Tổng các phần tử chia hết cho 3 là: "+tong);
+        TbCong=tong/diem;
+        System.out.println("Trung bình cộng của các số là :"+TbCong);
+        
+    }
 }
